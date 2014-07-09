90a1aeac8fbcd4564305cb01eaf93acb3ce3cd0d
PinkingS <405768386@qq.com>
2014-07-09 10:37:22 +0800

    CTCI 2.5

--- 2-5-addList.cpp
+++ 2-5-addList.cpp
@@ -56,16 +56,73 @@ ListNode* addList(ListNode *l1, ListNode *l2) {
 	return dummy.next;
 }
 
+int getLen(ListNode *head) {
+	int len = 0;
+	while(head) {
+		++len;
+		head = head->next;
+	}
+	return len;
+}
+
+ListNode* padList(ListNode *head, int padding) {
+	ListNode *newHead = head;
+	while(padding) {
+		ListNode *node = new ListNode(0);
+		node->next = newHead;
+		newHead = node;
+		--padding;
+	}
+	return newHead;
+}
+
+ListNode* addListReverseRe(ListNode *l1, ListNode *l2, int &carry) {
+	if(!l1 && !l2)
+		return NULL;
+	
+	ListNode *head = addListReverseRe(l1->next, l2->next, carry);
+	int sum = carry + l1->val + l2->val;
+	ListNode *node = new ListNode(sum % 10);
+	node->next = head;
+	carry = sum / 10;
+	return node;
+}
+
+ListNode* addListReverse(ListNode *l1, ListNode *l2) {
+	int len1 = getLen(l1);
+	int len2 = getLen(l2);
+
+	if(len1 < len2)
+		l1 = padList(l1, len2 - len1);
+	else
+		l2 = padList(l2, len1 - len2);
+
+	int carry = 0;
+	ListNode *sumList = addListReverseRe(l1, l2, carry);
+	
+	if(carry == 0)
+		return sumList;
+	else {
+		ListNode *res = new ListNode(carry);
+		res->next = sumList;
+		return res;
+	}
+		
+}
+
 int main() {
 	int a[] = {7, 3, 2};
 	ListNode *l1 = createList(a, 3);
 	showList(l1);
 
-	int b[] = {5, 1, 9, 1, 1};
-	ListNode *l2 = createList(b, 5);
+	int b[] = {9, 1, 1};
+	ListNode *l2 = createList(b, 3);
 	
 	ListNode *res = addList(NULL, l2);
 	showList(res);
+
+	ListNode *res2 = addListReverse(l1, l2);
+	showList(res2);
 
 	return 0;
 }
