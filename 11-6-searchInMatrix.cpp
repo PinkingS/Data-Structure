5dda54652328ab14bff33038e8d6b90233ff4857
PinkingS <405768386@qq.com>
2014-08-15 10:30:32 +0800

    CTCI 11.6 search in sorted matrix

---
+++ 11-6-searchInMatrix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int d[20][20];
+
+int search(int m, int n, int x) {
+	int r = 0, c = n-1;
+	while(r < m && c >= 0) {
+		if(d[r][c] == x) return r*n+c;
+		else if(d[r][c] < x) 
+			++r;
+		else
+			--c;
+	}
+	return -1;
+}
+
+int main() {
+	freopen("11.6.in", "r", stdin);
+	int m,n;
+	cin >> m >> n;
+	for(int i = 0; i < m; ++i)
+		for(int j = 0; j < n; ++j)
+			cin >> d[i][j];
+
+	int k = search(m, n, 13);
+	if(k == -1) cout << "NOT FOUND!" << endl;
+	else cout << k/n << " " << k%n << endl;
+	fclose(stdin);
+	return 0;
+}
