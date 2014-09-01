542f8481ea48b0ca44b454f2b6720ed9958127d3
PinkingS <405768386@qq.com>
2014-09-01 16:10:40 +0800

    add two new solutions

--- leetcode/permutation.cpp
+++ leetcode/permutation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 void permuteRe(vector<int> &num, vector<vector<int> > &res, vector<int> per, vector<bool> &used) {
@@ -34,12 +35,39 @@ vector<vector<int> > permute(vector<int> &num) {
 	return res;
 }
 
+vector<vector<int> > permute_2(vector<int> &num) {
+	vector<vector<int> > res;
+	sort(num.begin(), num.end());
+	res.push_back(num);
+
+	while(next_permutation(num.begin(), num.end()))
+		res.push_back(num);
+	return res;
+}
+
+void permute_3Re(vector<int> &num, int i, vector<vector<int> > &res) {
+	if(i == num.size()) {
+		res.push_back(num);
+		return;
+	}
+	for(int j = i; j < num.size(); ++j) {
+		swap(num[i], num[j]);
+		permute_3Re(num, i+1, res);
+		swap(num[i], num[j]);
+	}
+}
+
+vector<vector<int> > permute_3(vector<int> &num) {
+	vector<vector<int> > res;
+	permute_3Re(num, 0, res);
+	return res;
+}
 
 
 int main() {
 	int a[] = {1, 2, 3};
 	vector<int> num(a, a+3);
-	vector<vector<int> > res = permute(num);
+	vector<vector<int> > res = permute_3(num);
 	for(int i = 0; i < res.size(); ++i) {
 		for(int j = 0; j < res[i].size(); ++j)
 			cout << res[i][j] << " ";
