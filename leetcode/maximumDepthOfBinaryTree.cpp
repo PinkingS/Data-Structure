9b156bc565938ca03bf766e45ce7ff76bfdfe7d3
PinkingS <405768386@qq.com>
2014-09-13 10:30:33 +0800

    tree

---
+++ leetcode/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <cstdlib>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+// build tree from string ...
+vector<string> split(string s) {
+	vector<string> ret;
+	int last = 0;
+	int index = s.find_first_of(',', last);
+	while(index != -1) {
+		ret.push_back(s.substr(last, index-last));
+		last = index+1;
+		index = s.find_first_of(',', last);
+	}
+	
+	ret.push_back(s.substr(last));
+	
+	return ret;
+}
+
+TreeNode* constructNode(string s) {
+	if(s == "#")
+		return NULL;
+	else
+		return new TreeNode(atoi(s.c_str()));
+}
+
+TreeNode* createTree(string s) {
+	vector<string> tree = split(s);
+	queue<TreeNode *> q;
+	TreeNode *root = constructNode(tree[0]);
+	int idx = 1;
+	q.push(root);
+	while(!q.empty()) {
+		TreeNode *node = q.front();q.pop();
+		if(node == NULL)
+			continue;
+		
+		if(idx == tree.size())
+			break;
+
+		TreeNode* leftNode = constructNode(tree[idx]);
+		node->left = leftNode;
+		q.push(leftNode);
+		idx++;
+		if(idx == tree.size())
+			break;
+
+		TreeNode* rightNode = constructNode(tree[idx]);
+		node->right = rightNode;
+		q.push(rightNode);
+		idx++;
+	}
+	return root;
+}
+
+
+// delete tree...
+void deleteTree(TreeNode *root) {
+	if(!root)
+		return;
+	deleteTree(root->left);
+	deleteTree(root->right);
+	delete root;
+}
+
+
+// show tree in level order ...
+void showTree(TreeNode *root) {
+	queue<TreeNode*> q;
+	if(!root)
+		return;
+	q.push(root);
+	q.push(NULL);
+	while(!q.empty()) {
+		TreeNode *node = q.front();q.pop();
+		if(node == NULL) {
+			cout << endl;
+			if(q.empty())
+				return;
+			q.push(NULL);
+		}
+		else {
+			cout << node->val << " ";
+			if(node->left)
+				q.push(node->left);
+			if(node->right)
+				q.push(node->right);		
+		}
+	}
+}
+
+int maxDepth(TreeNode *root) {
+	if(!root)
+		return 0;
+	int left = maxDepth(root->left);
+	int right = maxDepth(root->right);
+	return max(left, right)+1;
+}
+
+
+int main() {
+	string s = "1,2,3,#,#,4,#,#,5";
+	TreeNode *root = createTree(s);
+	showTree(root);
+	cout << maxDepth(root) << endl;
+	deleteTree(root);
+	return 0;
+}
