917ac332f04db2e1bdf5bbaf2868655828d377fa
PinkingS <405768386@qq.com>
2014-09-15 15:43:48 +0800

    tree DP

---
+++ leetcode/uniqueBinarySearchTrees.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <cstdlib>
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <cstring>
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+// build tree from string ...
+vector<string> split(string s) {
+	vector<string> ret;
+	int last = 0;
+	int index = s.find_first_of(',', last);
+	while(index != -1) {
+		ret.push_back(s.substr(last, index-last));
+		last = index+1;
+		index = s.find_first_of(',', last);
+	}
+	
+	ret.push_back(s.substr(last));
+	
+	return ret;
+}
+
+TreeNode* constructNode(string s) {
+	if(s == "#")
+		return NULL;
+	else
+		return new TreeNode(atoi(s.c_str()));
+}
+
+TreeNode* createTree(string s) {
+	vector<string> tree = split(s);
+	queue<TreeNode *> q;
+	TreeNode *root = constructNode(tree[0]);
+	int idx = 1;
+	q.push(root);
+	while(!q.empty()) {
+		TreeNode *node = q.front();q.pop();
+		if(node == NULL)
+			continue;
+		
+		if(idx == tree.size())
+			break;
+
+		TreeNode* leftNode = constructNode(tree[idx]);
+		node->left = leftNode;
+		q.push(leftNode);
+		idx++;
+		if(idx == tree.size())
+			break;
+
+		TreeNode* rightNode = constructNode(tree[idx]);
+		node->right = rightNode;
+		q.push(rightNode);
+		idx++;
+	}
+	return root;
+}
+
+
+// delete tree...
+void deleteTree(TreeNode *root) {
+	if(!root)
+		return;
+	deleteTree(root->left);
+	deleteTree(root->right);
+	delete root;
+}
+
+
+// show tree in level order ...
+void showTree(TreeNode *root) {
+	queue<TreeNode*> q;
+	if(!root)
+		return;
+	q.push(root);
+	q.push(NULL);
+	while(!q.empty()) {
+		TreeNode *node = q.front();q.pop();
+		if(node == NULL) {
+			cout << endl;
+			if(q.empty())
+				return;
+			q.push(NULL);
+		}
+		else {
+			cout << node->val << " ";
+			if(node->left)
+				q.push(node->left);
+			if(node->right)
+				q.push(node->right);		
+		}
+	}
+}
+
+int numTrees(int n) {
+	int dp[n+1];
+	memset(dp, 0, sizeof(dp));
+	dp[0] = 1;
+	for(int i = 1; i <= n; ++i)
+		for(int j = 0; j < i; ++j) {
+			dp[i] += dp[j] * dp[i-j-1];
+		}
+	return dp[n];
+}
+
+
+int main() {
+	cout << numTrees(3) << endl;
+	return 0;
+}
---
+++ leetcode/uniqueBinarySearchTreesII.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <cstdlib>
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <cstring>
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+// build tree from string ...
+vector<string> split(string s) {
+	vector<string> ret;
+	int last = 0;
+	int index = s.find_first_of(',', last);
+	while(index != -1) {
+		ret.push_back(s.substr(last, index-last));
+		last = index+1;
+		index = s.find_first_of(',', last);
+	}
+	
+	ret.push_back(s.substr(last));
+	
+	return ret;
+}
+
+TreeNode* constructNode(string s) {
+	if(s == "#")
+		return NULL;
+	else
+		return new TreeNode(atoi(s.c_str()));
+}
+
+TreeNode* createTree(string s) {
+	vector<string> tree = split(s);
+	queue<TreeNode *> q;
+	TreeNode *root = constructNode(tree[0]);
+	int idx = 1;
+	q.push(root);
+	while(!q.empty()) {
+		TreeNode *node = q.front();q.pop();
+		if(node == NULL)
+			continue;
+		
+		if(idx == tree.size())
+			break;
+
+		TreeNode* leftNode = constructNode(tree[idx]);
+		node->left = leftNode;
+		q.push(leftNode);
+		idx++;
+		if(idx == tree.size())
+			break;
+
+		TreeNode* rightNode = constructNode(tree[idx]);
+		node->right = rightNode;
+		q.push(rightNode);
+		idx++;
+	}
+	return root;
+}
+
+
+// delete tree...
+void deleteTree(TreeNode *root) {
+	if(!root)
+		return;
+	deleteTree(root->left);
+	deleteTree(root->right);
+	delete root;
+}
+
+
+// show tree in level order ...
+void showTree(TreeNode *root) {
+	queue<TreeNode*> q;
+	if(!root)
+		return;
+	q.push(root);
+	q.push(NULL);
+	while(!q.empty()) {
+		TreeNode *node = q.front();q.pop();
+		if(node == NULL) {
+			cout << endl;
+			if(q.empty())
+				return;
+			q.push(NULL);
+		}
+		else {
+			cout << node->val << " ";
+			if(node->left)
+				q.push(node->left);
+			if(node->right)
+				q.push(node->right);		
+		}
+	}
+}
+
+
+vector<TreeNode *> generateTreesRe(int l, int r) {
+	vector<TreeNode *> res;
+	if(l > r) {
+		res.push_back(NULL);
+		return res;
+	}
+	
+	for(int k = l; k <= r; ++k) {
+		vector<TreeNode *> leftTrees = generateTreesRe(l, k-1);
+		vector<TreeNode *> rightTrees = generateTreesRe(k+1, r);
+		for(int i = 0; i < leftTrees.size(); ++i)
+			for(int j = 0; j < rightTrees.size(); ++j) {
+				TreeNode *root = new TreeNode(k);
+				root->left = leftTrees[i];
+				root->right = rightTrees[j];
+				res.push_back(root);
+			}
+	}
+	return res;
+}
+
+vector<TreeNode *> generateTrees(int n) {
+	return generateTreesRe(1, n);
+}
+
+
+int main() {
+	int n = 3;
+	vector<TreeNode *> trees = generateTrees(n);
+	for(int i = 0; i < trees.size(); ++i) {
+		showTree(trees[i]);
+		cout << endl;
+	}
+	return 0;
+}
